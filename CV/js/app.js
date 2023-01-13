d97414781b01827db3332e87692b436b68ff8e78
Mauricio Alvarez <aqcmauricio@gmail.com>
2023-01-13 11:40:31 -0500

    Se ajusto la foto en el alert al cargar la página por primera vez

--- CV/js/app.js
+++ CV/js/app.js
@@ -2,12 +2,25 @@
 
 $(document).ready(()=>{
     Swal.fire({
-        title: 'Quiero contarte que esta página aún está en desarrollo.',
-        text: '¡Gracias por tu visita!',
-        imageUrl: './assets/foto.png',
-        imageWidth: 200,
+        title: 'Quiero contarte que esta página aún está en proceso de desarrollo.',
+        imageUrl: './assets/foto4.png',
+        imageWidth: 40,
         imageHeight: 250,
         imageAlt: 'Custom image',
+        html: '¡Gracias por tu visita! <b></b> segundos.',
+        background: '#fff',
+        timer: 10000,
+        timerProgressBar: true,
+        didOpen: () => {
+            Swal.showLoading()
+            const b = Swal.getHtmlContainer().querySelector('b')
+            timerInterval = setInterval(() => {
+                b.textContent = Math.round(Swal.getTimerLeft() / 1000,0)
+            }, 1000)
+        },
+        willClose: () => {
+            clearInterval(timerInterval)
+        }
     });
     $('body').on({'mousemove' : function( e ){
         let clientX = e.originalEvent.clientX;
