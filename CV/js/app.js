5a0ad44ad0aeef6a38f61adcd75696cb16758d9c
Mauricio Alvarez <aqcmauricio@gmail.com>
2023-01-16 18:31:32 -0500

    language implementation two

--- CV/js/app.js
+++ CV/js/app.js
@@ -15,33 +15,42 @@ $(document).ready(()=>{
 
     let timerInterval;
     let rutaImagen;
+    let mensaje;
+    let htmlMensaje;
+    let segundos;
     if ($(".check")[0].checked){
         rutaImagen = '../assets/foto4.png';
-        $(".es").css('color', '#05F4FE');
-        $(".en").css('color', '#015F63');
-        $(".h3").css('font-size', '3.5em');
+        mensaje = 'Quiero contarte que esta página aún está en proceso de desarrollo.';
+        htmlMensaje = '¡Gracias por tu visita! <b></b>'
+        segundos = ' segundos.'
+        $('.es').css('color', '#05F4FE');
+        $('.en').css('color', '#015F63');
+        $('.h3').css('font-size', '3.5em');
     } else {
         rutaImagen = './assets/foto4.png';
-        $(".en").css('color', '#05F4FE');
-        $(".es").css('color', '#015F63');
+        mensaje = 'This page is still in developing process.';
+        htmlMensaje = 'Thanks for your visit! <b></b>'
+        segundos = ' seconds.'
+        $('.en').css('color', '#05F4FE');
+        $('.es').css('color', '#015F63');
     }
     Swal.fire({
         position: 'bottom-end',
-        title: 'Quiero contarte que esta página aún está en proceso de desarrollo.',
+        title: mensaje,
         imageUrl: rutaImagen,
         imageWidth: 40,
         imageHeight: 200,
         imageAlt: 'Custom image',
-        html: '¡Gracias por tu visita! <b></b>',
+        html: htmlMensaje,
         color: '#993',
         background: '#1340',
-        timer: 1000,
+        timer: 5000,
         timerProgressBar: true,
         didOpen: () => {
             Swal.showLoading()
             const b = Swal.getHtmlContainer().querySelector('b')
             timerInterval = setInterval(() => {
-                b.textContent = Math.round(Swal.getTimerLeft() / 1000, 0) + " segundos."
+                b.textContent = ' -> ' + Math.round(Swal.getTimerLeft() / 1000, 0) + segundos
             }, 1000)
         },
         willClose: () => {
